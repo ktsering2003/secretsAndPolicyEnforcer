5bc01db3cc06722b8d10e217cab4a6f15827d438
Kunsang Tsering <cozyboykt@Kunsangs-MacBook-Air.local>
2025-06-11 22:19:47 -0400

    Task 1 completed: Added SecretScanner and README

---
+++ src/SecretScanner.java
@@ -0,0 +1,6 @@
+public class SecretScanner {
+    public static void main(String[] args) {
+        System.out.println("Hello from SecretScanner!");
+        // Rest of my scanning code
+    }
+}
---
+++ test/TestFile.java
@@ -0,0 +1,15 @@
+
+public class testfile {
+    public static void main(String[] args) {
+    
+        String token = "abc123456789";
+        String awskey = "AKIAIOSFODNN7EXAMPLE";
+        eval("alert('You got Hacked!')");
+    }
+    
+    public static void eval(String script) {
+        System.out.println(" RISKY: " + command);
+    }
+}
+
+
